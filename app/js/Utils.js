7db8b73b8ea12b0e7ce722b9116ca117d43fd34d
erluzi <747877078@qq.com>
2019-10-18 12:14:11 +0800

    fix: format code

--- app/js/Utils.js
+++ app/js/Utils.js
@@ -1,53 +1,52 @@
 // 将base64的applicationServerKey转换成UInt8Array
 function urlBase64ToUnit8Array(base64String) {
-	var padding = '='.repeat((4 - base64String.length % 4) % 4);
-	var base64 = (base64String + padding)
-	.replace(/\-/g, '+')
-	.replace(/_/g, '/');
-	var rawData = window.atob(base64);
-	var outputArray = new Uint8Array(rawData.length);
-	for (var i = 0, max = rawData.length; i < max; ++i) {
-		outputArray[i] = rawData.charCodeAt(i);
-	}
-	return outputArray;
+  var padding = '='.repeat((4 - base64String.length % 4) % 4);
+  var base64 = (base64String + padding)
+  .replace(/\-/g, '+')
+  .replace(/_/g, '/');
+  var rawData = window.atob(base64);
+  var outputArray = new Uint8Array(rawData.length);
+  for (var i = 0, max = rawData.length; i < max; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
 
 function ab2str(buf) {
-    // var enc = new TextDecoder('utf-8');
-    var enc = new TextDecoder('iso-8859-2');
-    var arr = new Uint8Array(buf);
-    console.log(enc.decode(arr));
-    // return enc.decode(arr);
-
-    // return String.fromCharCode.apply(null, new Uint16Array(toUint8Array));
-    return String.fromCharCode.apply(null, new Uint8Array(buf));
+  // var enc = new TextDecoder('utf-8');
+  var enc = new TextDecoder('iso-8859-2');
+  var arr = new Uint8Array(buf);
+  console.log(enc.decode(arr));
+  // return enc.decode(arr);
+  // return String.fromCharCode.apply(null, new Uint16Array(toUint8Array));
+  return String.fromCharCode.apply(null, new Uint8Array(buf));
 }
 
 function notification({title, useNative}) {
-	if(useNative){
-        var n = new Notification(title, {
-            tag: 'normal',
-            icon: '/app/img/favicon.ico',
-            body: 'native!'
-        });
-        n.addEventListener('click', function(e){
-            console.log(e)
-        });
-        setTimeout(n.close.bind(n), 3000);
-	}else{
-        navigator.serviceWorker.ready.then(reg => {
-            reg.showNotification(title, {
-                icon: '/app/img/favicon.ico',
-                body: 'can u see?'
-            });
-        });
-	}
+  if (useNative) {
+    var n = new Notification(title, {
+      tag: 'normal',
+      icon: '/app/img/favicon.ico',
+      body: 'native!'
+    });
+    n.addEventListener('click', function (e) {
+      console.log(e)
+    });
+    setTimeout(n.close.bind(n), 3000);
+  } else {
+    navigator.serviceWorker.ready.then(reg => {
+      reg.showNotification(title, {
+        icon: '/app/img/favicon.ico',
+        body: 'can u see?'
+      });
+    });
+  }
 }
 
 function appendMsgDom(info) {
-    let p = document.createElement('p');
-    p.textContent = info;
-    document.querySelector('#infos').appendChild(p);
+  let p = document.createElement('p');
+  p.textContent = info;
+  document.querySelector('#infos').appendChild(p);
 }
 
 export {urlBase64ToUnit8Array, ab2str, notification, appendMsgDom}
--- app/js/initDom.js
+++ app/js/initDom.js
@@ -2,27 +2,27 @@ import {notification} from './Utils.js'
 import {api_request} from './xhr.js'
 
 export function initDom() {
-	let t_notification = document.querySelector('.t-notification');
-	let t_api = document.querySelector('.t-api');
-	let t_api_no = document.querySelector('.t-api-no');
+  let t_notification = document.querySelector('.t-notification');
+  let t_api = document.querySelector('.t-api');
+  let t_api_no = document.querySelector('.t-api-no');
 
-	t_notification.addEventListener('click', e => {
-		notification({title: 'test...', useNative: true})
-	});
-	t_api.addEventListener('click', e => {
-		api_request('/api_report', 'POST', {type: 0, code: 1, content: 'tte'}).then(data => console.log(data))
-	});
-	t_api_no.addEventListener('click', e => {
-		api_request('/list', 'POST', {type: 0}).then(data => console.log(data))
-	});
+  t_notification.addEventListener('click', e => {
+    notification({title: 'test...', useNative: true})
+  });
+  t_api.addEventListener('click', e => {
+    api_request('/api_report', 'POST', {type: 0, code: 1, content: 'tte'}).then(data => console.log(data))
+  });
+  t_api_no.addEventListener('click', e => {
+    api_request('/list', 'POST', {type: 0}).then(data => console.log(data))
+  });
 }
 
 export function initDomWithPwa(pwa) {
-    let re_subscribe = document.querySelector('.re-subscribe');
+  let re_subscribe = document.querySelector('.re-subscribe');
 
-    re_subscribe.addEventListener('click', e => {
-        pwa.cancelPush().then(() => {
-        	pwa.initPush()
-		})
+  re_subscribe.addEventListener('click', e => {
+    pwa.cancelPush().then(() => {
+      pwa.initPush()
     })
+  })
 }
