6ba743aedb6dcea6761842c4481b970fe39dda21
erluzi <747877078@qq.com>
2020-12-01 11:10:08 +0800

    fix: code format

--- app/js/PwaInit.js
+++ app/js/PwaInit.js
@@ -1,155 +1,160 @@
-import {urlBase64ToUnit8Array, appendMsgDom} from './Utils.js';
-import {api_request} from "./xhr";
+import {urlBase64ToUnit8Array, appendMsgDom} from './Utils.js'
+import {api_request} from './xhr'
 
-const serverApi = 'http://47.93.35.239/net_set';
+const serverApi = 'http://47.93.35.239/net_set'
 // const serverApi = 'http://localhost:8000/net_set';
 // const serverApi = 'http://localhost:8083/net_set';
-const publicKey = 'BKmzm3addDa0_hQNkJ0Readn9V2-mIhtdNvfq_yOzYpY14hGhSGJ5ZD4flqSCBDEwlwxjiaLHparbg2n0h0gxOU';
+const publicKey = 'BKmzm3addDa0_hQNkJ0Readn9V2-mIhtdNvfq_yOzYpY14hGhSGJ5ZD4flqSCBDEwlwxjiaLHparbg2n0h0gxOU'
 
 class Pwa {
-	constructor({elAddTo}){
-		this.dfdPrompt = null;
-		this.elAddTo = elAddTo;
-	}
-	initSW() {
-		if('serviceWorker' in navigator){
-			navigator.serviceWorker.register('/sw.js', {scope: '/'})
-				.then(registration => {
-                    appendMsgDom(`ServiceWorker登记成功，范围为${registration.scope}`);
-				})
-				.catch(function (err) {
-                    appendMsgDom('ServiceWorker登记失败：');
-                    appendMsgDom(err);
-				});
-			//监听sw消息
-            navigator.serviceWorker.addEventListener("message", event => {
-                appendMsgDom(`msg received from sw : ${event.data}`);
-            });
-        }else{
-			alert('not support serviceWorker')
-		}
-	}
-	initPush() {
-		if('PushManager' in window){
-			navigator.serviceWorker.ready.then(reg => {
-                reg.pushManager.getSubscription().then(subscription => {
-                	//未订阅
-					if(!subscription){
-                        appendMsgDom('begin subscribe...');
-						this.subscribe(reg)
-					}else{
-                        appendMsgDom('already subscribed!!!');
-					}
-                });
-			});
-		}else{
-			alert('not support PushManager')
-		}
-	}
-	cancelPush(){
-		return new Promise((resolve, reject) => {
-            navigator.serviceWorker.ready.then(reg => {
-                reg.pushManager.getSubscription().then(subscription => {
-                    if(subscription){
-                        subscription.unsubscribe().then(data => {
-                            appendMsgDom('cancel subscribe success');
-                            resolve()
-                        })
-						.catch(e => {
-							appendMsgDom('cancel subscribe fail');
-							appendMsgDom(e);
-                            reject(e)
-						});
-                    }else{
-                        resolve()
-					}
-                });
-            });
-		})
-	}
-	initNotification() {
-		// return new Promise(function (resolve, reject) {
-		// 	let permissionResult = Notification.requestPermission(function (result) {
-		// 		// 旧版本
-		// 		resolve(result);
-		// 	});
-		// 	if (permissionResult) {
-		// 		// 新版本
-		// 		permissionResult.then(resolve, reject);
-		// 	}
-		// })
-        Notification.requestPermission().then(permissionResult => {
-			if (permissionResult === 'granted') {
-                appendMsgDom('Notification is supported');
-			} else {
-                alert('not support Notification')
-			}
-		});
-	}
-	initAddToScreen(){
-		let button = document.querySelector(this.elAddTo);
+  constructor({elAddTo}) {
+    this.dfdPrompt = null
+    this.elAddTo = elAddTo
+  }
 
-		window.addEventListener('beforeinstallprompt', e => {
-			e.userChoice.then(choiceResult => handleResult(choiceResult, e));
-			// 阻止默认事件
-			// e.preventDefault();
-			// return false;
-		});
+  initSW() {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js', {scope: '/'})
+          .then(registration => {
+            appendMsgDom(`ServiceWorker登记成功，范围为${registration.scope}`)
+          })
+          .catch(function (err) {
+            appendMsgDom('ServiceWorker登记失败：')
+            appendMsgDom(err)
+          })
+      //监听sw消息
+      navigator.serviceWorker.addEventListener('message', event => {
+        appendMsgDom(`msg received from sw : ${event.data}`)
+      })
+    } else {
+      alert('not support serviceWorker')
+    }
+  }
 
-		button.addEventListener('click', () => {
-			if (this.dfdPrompt == null) return;
-			// 通过按钮点击事件触发横幅显示
-			this.dfdPrompt.prompt();
-			// 监控用户的安装行为
-			this.dfdPrompt.userChoice.then(choiceResult => handleResult(choiceResult));
-		});
+  initPush() {
+    if ('PushManager' in window) {
+      navigator.serviceWorker.ready.then(reg => {
+        reg.pushManager.getSubscription().then(subscription => {
+          //未订阅
+          if (!subscription) {
+            appendMsgDom('begin subscribe...')
+            this.subscribe(reg)
+          } else {
+            appendMsgDom('already subscribed!!!')
+          }
+        })
+      })
+    } else {
+      alert('not support PushManager')
+    }
+  }
 
-		function handleResult(choiceResult, e){
-			if (choiceResult.outcome === 'dismissed') {
-                appendMsgDom('用户取消安装应用');
-				// 存储事件
-				if(!this.dfdPrompt && e) this.dfdPrompt = e;
-				button.style.display = 'block';
-			}
-			else {
-                appendMsgDom('用户安装了应用');
-				this.dfdPrompt = null;
-				button.style.display = 'none';
-			}
-		}
-	}
-	subscribe(serviceWorkerReg) {
-		//浏览器向FCM服务(火狐不同)请求生成subscription
-		serviceWorkerReg.pushManager.subscribe({
-			userVisibleOnly: true,
-			applicationServerKey: urlBase64ToUnit8Array(publicKey)
-		})
-		.then(subscription => {
-			// console.log(subscription.endpoint);
-            // console.log(subscription.getKey('p256dh'));
-            // console.log(subscription.getKey('auth'));
-            // console.log(subscription.toJSON());
-            // console.log(subscription.toJSON().keys.p256dh);
-            // console.log(subscription.toJSON().keys.auth);
-            // 3. 发送推送订阅对象到服务器，具体实现中发送请求到后端api
-            appendMsgDom('browser subscribe success, send subscription to server');
-            return api_request(serverApi, 'post', {
-				endpoint: subscription.endpoint,
-				p256dh: subscription.toJSON().keys.p256dh,
-				auth: subscription.toJSON().keys.auth,
+  cancelPush() {
+    return new Promise((resolve, reject) => {
+      navigator.serviceWorker.ready.then(reg => {
+        reg.pushManager.getSubscription().then(subscription => {
+          if (subscription) {
+            subscription.unsubscribe().then(data => {
+              appendMsgDom('cancel subscribe success')
+              resolve()
             })
-		})
-		.catch(e => {
-            appendMsgDom('browser subscribe fail');
-            appendMsgDom(e);
-			console.warn(e);
-			if (Notification.permission === 'denied') {
-				// 用户拒绝了订阅请求
-                appendMsgDom('用户拒绝了订阅请求')
-			}
-		});
-	}
+                .catch(e => {
+                  appendMsgDom('cancel subscribe fail')
+                  appendMsgDom(e)
+                  reject(e)
+                })
+          } else {
+            resolve()
+          }
+        })
+      })
+    })
+  }
+
+  initNotification() {
+    // return new Promise(function (resolve, reject) {
+    // 	let permissionResult = Notification.requestPermission(function (result) {
+    // 		// 旧版本
+    // 		resolve(result);
+    // 	});
+    // 	if (permissionResult) {
+    // 		// 新版本
+    // 		permissionResult.then(resolve, reject);
+    // 	}
+    // })
+    Notification.requestPermission().then(permissionResult => {
+      if (permissionResult === 'granted') {
+        appendMsgDom('Notification is supported')
+      } else {
+        alert('not support Notification')
+      }
+    })
+  }
+
+  initAddToScreen() {
+    let button = document.querySelector(this.elAddTo)
+
+    window.addEventListener('beforeinstallprompt', e => {
+      e.userChoice.then(choiceResult => handleResult(choiceResult, e))
+      // 阻止默认事件
+      // e.preventDefault();
+      // return false;
+    })
+
+    button.addEventListener('click', () => {
+      if (this.dfdPrompt == null) return
+      // 通过按钮点击事件触发横幅显示
+      this.dfdPrompt.prompt()
+      // 监控用户的安装行为
+      this.dfdPrompt.userChoice.then(choiceResult => handleResult(choiceResult))
+    })
+
+    function handleResult(choiceResult, e) {
+      if (choiceResult.outcome === 'dismissed') {
+        appendMsgDom('用户取消安装应用')
+        // 存储事件
+        if (!this.dfdPrompt && e) this.dfdPrompt = e
+        button.style.display = 'block'
+      } else {
+        appendMsgDom('用户安装了应用')
+        this.dfdPrompt = null
+        button.style.display = 'none'
+      }
+    }
+  }
+
+  subscribe(serviceWorkerReg) {
+    //浏览器向FCM服务(火狐不同)请求生成subscription
+    serviceWorkerReg.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUnit8Array(publicKey),
+    })
+        .then(subscription => {
+          // console.log(subscription.endpoint);
+          // console.log(subscription.getKey('p256dh'));
+          // console.log(subscription.getKey('auth'));
+          // console.log(subscription.toJSON());
+          // console.log(subscription.toJSON().keys.p256dh);
+          // console.log(subscription.toJSON().keys.auth);
+          // 3. 发送推送订阅对象到服务器，具体实现中发送请求到后端api
+          appendMsgDom('browser subscribe success, send subscription to server')
+          return api_request(serverApi, 'post', {
+            endpoint: subscription.endpoint,
+            p256dh: subscription.toJSON().keys.p256dh,
+            auth: subscription.toJSON().keys.auth,
+          })
+        })
+        .catch(e => {
+          appendMsgDom('browser subscribe fail')
+          appendMsgDom(e)
+          console.warn(e)
+          if (Notification.permission === 'denied') {
+            // 用户拒绝了订阅请求
+            appendMsgDom('用户拒绝了订阅请求')
+          }
+        })
+  }
 
 }
 
-export default Pwa;
+export default Pwa
--- sw.js
+++ sw.js
@@ -1,130 +1,130 @@
-importScripts('./app/js/swImport.js');
+importScripts('./app/js/swImport.js')
 
-const V = '0.2';
+const V = '0.2'
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        async function(){
-            let cache = await caches.open(CACHE_INIT);
-            await cache.addAll(InitCacheFiles);
-            console.log("SW installed");
-            return self.skipWaiting();//强制当前处在 waiting 状态的 Service Worker 进入 activate 状态
-        }()
-    );
-});
+  event.waitUntil(
+      async function () {
+        let cache = await caches.open(CACHE_INIT)
+        await cache.addAll(InitCacheFiles)
+        console.log('SW installed')
+        return self.skipWaiting()//强制当前处在 waiting 状态的 Service Worker 进入 activate 状态
+      }(),
+  )
+})
 
 self.addEventListener('activate', event => {
-    console.log("SW activated");
-    event.waitUntil(
-        Promise.all([
-            // 更新客户端
-            self.clients.claim(),
-            // 清理旧版本
-            caches.keys().then(function (cacheList) {
-                // console.log('caches', caches)
-                // console.log('cacheList', cacheList)
-                return Promise.all(
-                    cacheList.map(function (cacheName) {
-                        if (cacheName !== CACHE_INIT) {
-                            return caches.delete(cacheName);
-                        }
-                    })
-                );
-            })
-        ])
-    );
-});
+  console.log('SW activated')
+  event.waitUntil(
+      Promise.all([
+        // 更新客户端
+        self.clients.claim(),
+        // 清理旧版本
+        caches.keys().then(function (cacheList) {
+          // console.log('caches', caches)
+          // console.log('cacheList', cacheList)
+          return Promise.all(
+              cacheList.map(function (cacheName) {
+                if (cacheName !== CACHE_INIT) {
+                  return caches.delete(cacheName)
+                }
+              }),
+          )
+        }),
+      ]),
+  )
+})
 
 self.addEventListener('fetch', event => {
-    let {destination, url, method} = event.request;
-    let request = event.request.clone();
-    sw_utils.postMsg(`Caught a fetch: ${url} - ${destination}`);
-    event.respondWith(
-        async function () {
-            if(!destination){
-                if(method !== 'GET' || url.match(/\/(sockjs-node|__webpack_hmr)/g)){
-                    return onlyNetwork(request)
-                }else if(url.match(/\/(api_|get)/g)){
-                    return networkFirst(request)
-                }else{
-                    return cacheFirst(request)
-                }
-            }else{
-                return cacheFirst(request)
-            }
-        }()
-    )
-});
+  let {destination, url, method} = event.request
+  let request = event.request.clone()
+  sw_utils.postMsg(`Caught a fetch: ${url} - ${destination}`)
+  event.respondWith(
+      async function () {
+        if (!destination) {
+          if (method !== 'GET' || url.match(/\/(sockjs-node|__webpack_hmr)/g)) {
+            return onlyNetwork(request)
+          } else if (url.match(/\/(api_|get)/g)) {
+            return networkFirst(request)
+          } else {
+            return cacheFirst(request)
+          }
+        } else {
+          return cacheFirst(request)
+        }
+      }(),
+  )
+})
 
 //监听推送
 self.addEventListener('push', event => {
-    console.log('get push');
-	if (event.data) {
-		let promiseChain = Promise.resolve(event.data.json()).then(data => self.registration.showNotification(data.title, {
-			icon: '/app/img/favicon.ico',
-			body: data.body,
-			tag: data.tag
-        }));
-		event.waitUntil(promiseChain);
-	}
-});
+  console.log('get push')
+  if (event.data) {
+    let promiseChain = Promise.resolve(event.data.json()).then(data => self.registration.showNotification(data.title, {
+      icon: '/app/img/favicon.ico',
+      body: data.body,
+      tag: data.tag,
+    }))
+    event.waitUntil(promiseChain)
+  }
+})
 
 //监听页面消息
 self.addEventListener('message', event => {
-    let msg = event.data;
-    console.log(`msg received from dom : ${msg}`);
-});
+  let msg = event.data
+  console.log(`msg received from dom : ${msg}`)
+})
 
 self.addEventListener('notificationclick', event => {
-    console.log('[Service Worker] Notification click Received.');
-    let notification = event.notification;
-    notification.close();
-    event.waitUntil(
-        clients.openWindow(notification.data.url)
-    );
-});
+  console.log('[Service Worker] Notification click Received.')
+  let notification = event.notification
+  notification.close()
+  event.waitUntil(
+      clients.openWindow(notification.data.url),
+  )
+})
 
 self.addEventListener('notificationclose', event => {
-    console.log('notificationclose')
-});
+  console.log('notificationclose')
+})
 
-async function onlyNetwork(request){
-    console.log('onlyNetwork');
-    let httpRes = await fetch(request);
-    if(!httpRes || httpRes.status !==200){
-        return sw_utils.getResponseNot200()
-    }
-    return httpRes;
+async function onlyNetwork(request) {
+  console.log('onlyNetwork')
+  let httpRes = await fetch(request)
+  if (!httpRes || httpRes.status !== 200) {
+    return sw_utils.getResponseNot200()
+  }
+  return httpRes
 }
 
 async function networkFirst(request) {
-    console.log('networkFirst');
-    let httpRes = await fetch(request);
-    if(!httpRes || httpRes.status !==200){
-        let response = await caches.match(request);
-        if(response) return response;
-        return sw_utils.getResponseNot200()
-    }
-    //请求成功, 则缓存
-    await cacheResponse(CACHE_NET_FIRST, request, httpRes.clone());
-    return httpRes;
+  console.log('networkFirst')
+  let httpRes = await fetch(request)
+  if (!httpRes || httpRes.status !== 200) {
+    let response = await caches.match(request)
+    if (response) return response
+    return sw_utils.getResponseNot200()
+  }
+  //请求成功, 则缓存
+  await cacheResponse(CACHE_NET_FIRST, request, httpRes.clone())
+  return httpRes
 }
 
 async function cacheFirst(request) {
-    console.log('cacheFirst');
-    let response = await caches.match(request);
-    if(response) return response;
-    console.log(`${request.url} no cache, fetch request!`);
-    let httpRes = await fetch(request);
-    if(!httpRes || httpRes.status !==200){
-        return sw_utils.getResponseNot200()
-    }
-    //请求成功, 再次缓存
-    await cacheResponse(CACHE_CACHE_FIRST, request, httpRes.clone());
-    return httpRes;
+  console.log('cacheFirst')
+  let response = await caches.match(request)
+  if (response) return response
+  console.log(`${request.url} no cache, fetch request!`)
+  let httpRes = await fetch(request)
+  if (!httpRes || httpRes.status !== 200) {
+    return sw_utils.getResponseNot200()
+  }
+  //请求成功, 再次缓存
+  await cacheResponse(CACHE_CACHE_FIRST, request, httpRes.clone())
+  return httpRes
 }
 
-async function cacheResponse(storeName, httpReq, httpRes){
-    let cache = await caches.open(storeName);
-    return cache.put(httpReq, httpRes);
+async function cacheResponse(storeName, httpReq, httpRes) {
+  let cache = await caches.open(storeName)
+  return cache.put(httpReq, httpRes)
 }
