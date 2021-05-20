b3b54f4cb30397285913022fd9db589a304ff9d3
erluzi <747877078@qq.com>
2021-05-20 17:09:41 +0800

    fix: re build

--- map-gd/map.js
+++ map-gd/map.js
@@ -4,13 +4,22 @@ import device from 'current-device'
 const isDesktop = device.desktop()
 
 window.mapLoaded = async function () {
+  let center
+
   if (isDesktop) {
     let result = await getCity()
     console.log('result:', result)
+    center = [result.bounds.southWest.lng, result.bounds.southWest.lat]
   } else {
     let position = await getPosition()
     console.log('position:', position)
   }
 
-  let map = new AMap.Map('container')
+  console.log('center:', center)
+
+  let map = new AMap.Map('container', {
+    viewMode: '3D',
+    zoom: 10,
+    ...(center ? {center} : {})
+  })
 }
